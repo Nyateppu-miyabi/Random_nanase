40433f4b939b1ee400b4bc7056978c87834ce963
Nyateppu-miyabi <89932633+Nyateppu-miyabi@users.noreply.github.com>
2021-09-04 15:24:59 +0900

    Update button_function.js
    コード短縮しました。添削してくれたけいすらさんに感謝です。

--- button_function.js
+++ button_function.js
@@ -1,25 +1,7 @@
-n = "nanase_voice_mp3".length;
-let str = 1;
-audioArr = [];
-
-var button = document.getElementById('button');
-  for (let i = 0; i < n; i++){
-    audioArr.push("nanase_voice_mp3/voice"+str+".mp3")
-    str = str + 1;
-  }
-
-var audio = new Audio();
-var get_random_voice = function(){
-    num = Math.floor(Math.random() * audioArr.length);
-  
-  if (num == audioArr.length) {
-    num = audioArr.length - 1;
-  }
-  audio.src = audioArr[num]
-  audio.currentTime = 0;
+const audio = new Audio();
+document.getElementById('button').addEventListener('click', () => {
+  const n = "nanase_voice_mp3".length;
+  const i = Math.floor(Math.random() * n + 1);
+  audio.src = `nanase_voice_mp3/voice${i}.mp3`;
   audio.play();
-};
-  
-button.addEventListener('click', function() {
-  get_random_voice();
-}, false);
+},false);
