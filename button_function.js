2cbea06e95f1198cacbc558906bdb56a84a40109
Nyateppu-miyabi <89932633+Nyateppu-miyabi@users.noreply.github.com>
2022-05-17 21:29:45 +0900

    Update button_function.js

--- button_function.js
+++ button_function.js
@@ -1,7 +1,14 @@
+const img = document.getElementById('main');
 const audio = new Audio();
 document.getElementById('button').addEventListener('click', () => {
   const n = "nanase_voice_mp3".length;
   const i = Math.floor(Math.random() * n + 1);
   audio.src = `nanase_voice_mp3/voice${i}.mp3`;
+  imgChange();
   audio.play();
 },false);
+
+function imgChange(){
+  const p = Math.floor(Math.random() * 2) + 1;
+  img.src = `img/main_img${p}.png`;
+};
